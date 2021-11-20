248c3e1d60c667a1414fe665a88dca6d910b022e
EdGeber <egma@cin.ufpe.br>
2021-11-19 22:00:22 -0300

    Begin controladora

--- controladora.v
+++ controladora.v
@@ -1,9 +1,9 @@
 module controladora (
-    input wire clk, Overflow,
-    input wire [31:0] instruction,
-    output wire rst, PCWriteCond, PCWrite, EQorNE, GTorLT, WDSrc, MemRead_Write, IRWrite,
-    output wire RegWrite, RegALoad, RegBLoad, ALUSrcA, EPCWrite, ALUOSrc, ALUOutLoad,
-    output wire GLtMux, TwoBytes, Store, DivOrM, HiLoSrc, HiLoWrite,
+    input wire clk, Overflow, reset
+    input wire [5:0] opcode, funct
+    output wire PCWriteCond, PCWrite, EQorNE, GTorLT, WDSrc, MemRead_Write, IRWrite,
+                RegWrite, RegALoad, RegBLoad, ALUSrcA, EPCWrite, ALUOSrc, ALUOutLoad,
+                GLtMux, TwoBytes, Store, DivOrM, HiLoSrc, HiLoWrite,
     output wire [1:0] RegDst, ALUSrcB, ShiftQnt, ShiftReg,
     output wire [2:0] IorD, ALUOp, PCSrc, ShiftType,
     output wire [3:0] MemtoReg
@@ -12,5 +12,20 @@ module controladora (
     /*
      * caue: Estou com medo dessa controladora
      * bruno: Eu também
+     * eduardo: rs
      */
+
+    reg [6:0] state, next_state;
+
+    always @(posedge clk or negedge reset) begin
+        if(!reset)
+            state = 0;  // reset state
+        else
+            state = next_state;
+    end
+
+    always @(state) begin: OUTPUT LOGIC AND NEXT STATE LOGIC
+        
+    end
+
 endmodule
