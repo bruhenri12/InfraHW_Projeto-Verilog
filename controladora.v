d12b5e7261c5064c81caa78f8c9a504429eb132e
EdGeber <egma@cin.ufpe.br>
2021-11-20 17:16:56 -0300

    Update controladora.v

--- controladora.v
+++ controladora.v
@@ -80,7 +80,7 @@ module controladora (
                 MemtoReg <= 4'b0111;
                 RegWrite <= 1;
                 // next state
-                next_state = 1;
+                next_state <= 1;
             end
 
             1: begin: START
@@ -115,62 +115,61 @@ module controladora (
                 ShiftType     <= 0;
                 MemtoReg      <= 0;
                 // output
-                PCWrite = 1;
-                IorD    = 0;
-                MemRead_Write = 0;
-                ALUSrcA = 0;
-                ALUSrcB = 1;
-                PCSrc   = 0;
-                ALUOp   = 1;
+                PCWrite       <= 1;
+                IorD          <= 0;
+                MemRead_Write <= 0;
+                ALUSrcA       <= 0;
+                ALUSrcB       <= 1;
+                PCSrc         <= 0;
+                ALUOp         <= 1;
                 // next state
-                next_state = 2;
+                next_state <= 2;
             end
 
             2: begin
                 // output
-                IRWrite = 1;
-                PCWrite = 0;
-                MemRead_Write = 0;
+                IRWrite       <= 1;
+                PCWrite       <= 0;
                 // next state
-                next_state = 3;
+                next_state    <= 3;
             end
 
             3: begin
                 // output
-                IRWrite = 0;
+                IRWrite    <= 0;
                 // next state
-                next_state = 4;
+                next_state <= 4;
             end
 
             4: begin: END_OF_CICLOS_COMUNS
                 // output
-                ALUSrcA     = 0;
-                ALUSrcB     = 3;
-                ALUOp       = 1;
-                ALUOSrc     = 0;
-                RegALoad    = 1;
-                RegBLoad    = 1;
-                ALUOutWrite = 1;
+                ALUSrcA     <= 0;
+                ALUSrcB     <= 3;
+                ALUOp       <= 1;
+                ALUOSrc     <= 0;
+                RegALoad    <= 1;
+                RegBLoad    <= 1;
+                ALUOutWrite <= 1;
                 // next state
                 if((opcode == op0 && funct == add_funct) ||
                     opcode == addi_op ||
                     opcode == addiu_op)
-                    next_state = 5;
+                    next_state <= 5;
                 // else if ...
                 else // opcode inexistente
-                    next_state = 11;
+                    next_state <= 11;
             end
 
             5: begin
                 // output
-                ALUSrcA     = 1;
-                ALUSrcB     = 0;
-                ALUOp       = 1;
-                ALUOSrc     = 0;
-                ALUOutWrite = 1;
+                ALUSrcA     <= 1;
+                ALUSrcB     <= 0;
+                ALUOp       <= 1;
+                ALUOSrc     <= 0;
+                ALUOutWrite <= 1;
                 // next state
                 if(Overflow)
-                    next_state = 7;  // overflow
+                    next_state <= 7;  // overflow
                 else if(opcode == op0 && funct == add_funct)
                     next_state <= 6;  // add
                 else if(opcode == addi_op)
@@ -180,35 +179,25 @@ module controladora (
 
             6: begin: END_OF_ADD
                 // output
-                ALUOutWrite = 0;
-                MemtoReg    = 0;
-                RegDst      = 1;
-                RegWrite    = 1;
+                ALUOutWrite <= 0;
+                MemtoReg    <= 0;
+                RegDst      <= 1;
+                RegWrite    <= 1;
                 // next state
-                next_state = 1;  // start
+                next_state <= 1;  // start
             end
 
-            7: begin
+            7: begin: INICIO_OVERFLOW
                 // output
-                ALUOutWrite = 0;
-                ALUSrcA     = 0;
-                ALUSrcB     = 1;
-                ALUOp       = 3'b010;
-                EPCWrite    = 1;
-                IorD        = 3;
-                MemRead_Write     = 1;
+                ALUOutWrite   <= 0;
+                ALUSrcA       <= 0;
+                ALUSrcB       <= 1;
+                ALUOp         <= 3'b010;
+                EPCWrite      <= 1;
+                IorD          <= 3;
+                MemRead_Write <= 0;
                 // next state
-                next_state = 12;
-            end
-
-            8: begin: END_OF_ADDI
-                // output
-                ALUOutWrite = 0;
-                MemtoReg    = 0;
-                RegDst      = 0;
-                RegWrite    = 1;
-                // next_state
-                next_state = 1; // start
+                next_state <= 12;
             end
 
             8: begin: END_OF_ADDI
@@ -223,42 +212,41 @@ module controladora (
 
             11: begin: OPCODE_INEXISTENTE
                 // output
-                IorD     = 2;
-                MemRead_Write  = 1;
-                ALUSrcA  = 0;
-                ALUSrcB  = 1;
-                ALUOp    = 010;
-                EPCWrite = 1;
+                IorD           <= 2;
+                MemRead_Write  <= 0;
+                ALUSrcA        <= 0;
+                ALUSrcB        <= 1;
+                ALUOp          <= 010;
+                EPCWrite       <= 1;
                 // next state
-                next_state = 12;
+                next_state <= 12;
             end
 
             12: begin: TRATAMENTO_DE_EXCECAO_PADRAO
                 // output
-                EPCWrite = 0;
-                MemRead_Write  = 0;
+                EPCWrite       <= 0;
                 // next state
-                next_state = 13;
+                next_state <= 13;
             end
 
             13: begin
                 // output
-                MDRLoad = 1;
+                MDRLoad <= 1;
                 // next state
-                next_state = 14;
+                next_state <= 14;
             end
 
             14: begin: END_OF_TRATAMENTO_DE_EXCECAO_PADRAO
                 // output
-                MDRLoad  = 0;
-                Store    = 0;
-                TwoBytes = 0;
-                PCSrc    = 3;
-                PCWrite  = 1;
+                MDRLoad  <= 0;
+                Store    <= 0;
+                TwoBytes <= 0;
+                PCSrc    <= 3;
+                PCWrite  <= 1;
                 // next state
-                next_state = 1;
+                next_state <= 1;
             end
-            default: next_state = 0;
+            default: next_state <= 0;
         endcase
     end
 
