a6a79ea617d71240ffa9b2743f34348371d444c2
EdGeber <egma@cin.ufpe.br>
2021-11-19 21:34:29 -0300

    Add overwrite_block

---
+++ overwrite_block.v
@@ -0,0 +1,10 @@
+module overwrite_block(
+    input wire [31:0] written, writer,
+    input wire two_bytes,
+    output wire [31:0] result);
+
+    assign result = two_bytes ?
+                    {written[31:16], writer[15:0]} :
+                    {written[31:8], writer[7:0]}
+
+endmodule
