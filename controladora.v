cca2cda051d09f2fa32c91b9e4155059424ed7b7
EdGeber <egma@cin.ufpe.br>
2021-11-25 14:37:29 -0300

    Brings shifts from Bruno and add sram

--- controladora.v
+++ controladora.v
@@ -18,6 +18,13 @@ module controladora (
      * Sáb nov 2 às 15:38 - Cauê: E lá vamos nós.
     */
 
+    parameter  sll_funct  = 6'h0;	
+    parameter  sllv_funct = 6'h4;	
+    parameter  sra_funct  = 6'h3;	
+    parameter  srav_funct = 6'h7;	
+    parameter  srl_funct  = 6'h2;	
+    parameter  sram_op    = 6'b000001;	
+    parameter  lui_op     = 6'hf;
     parameter  add_funct  = 6'h20;
     parameter  and_funct  = 6'h24;
     parameter  div_funct  = 6'h1a;
@@ -28,7 +35,6 @@ module controladora (
     parameter  op0        = 6'h0;
 
     reg [7:0] state;
-
 
     always @(posedge clk) begin: NEXT_STATE_LOGIC
         case(state)
@@ -51,8 +57,16 @@ module controladora (
             4: begin: END_OF_CICLOS_COMUNS
                 if((opcode == op0) && (funct == add_funct))
                     state <= 5;  // add
+                else if((opcode == op0) && ((funct == sll_funct) || (funct == sra_funct) || (funct == srl_funct)))
+                    state = 20;  // sll, sra or srl
+                else if((opcode == op0) && ((funct == sllv_funct) || (funct == srav_funct)))
+                    state = 22;  // sllv, srav
+                else if(opcode == sram_op)
+                    state = 25;  // sram
+                else if(opcode == lui_op)
+                    state = 26;  // lui
                 else if((opcode == addi_op) || (opcode == addiu_op))
-                    state <= 73;  // addi or addiu
+                    state <= 73; // addi or addiu
                 else // opcode inexistente
                     state <= 11;
             end
@@ -85,6 +99,84 @@ module controladora (
             end
 
             14: begin: GO_TO_START_14
+                state <= 1;
+            end
+
+            25: begin: GO_TO_24
+                if(funct == sll_funct)
+                    state <= 26;
+                else if(funct == sra_funct)
+                    state <= 27;
+                else if(funct == srl_funct)
+                    state <= 28;
+                else
+                    state <= 0;
+            end
+
+            26: begin
+                state <= 29;
+            end
+
+            27: begin
+                state <= 29;
+            end
+
+            28: begin
+                state <= 29;
+            end
+
+            29: begin
+                state <= 1;
+            end
+
+            30: begin: GO_TO_23
+                if(funct == sllv_funct)
+                    state <= 31;
+                else if(funct == srav_funct)
+                    state <= 32;
+            end
+
+            31: begin
+                next_state <= 33;
+            end
+
+            32: begin
+                next_state <= 33;
+            end
+
+            33: begin: GO_TO_START_24
+                state <= 1;
+            end
+
+            36: begin
+                state <= 37;
+            end
+
+            37: begin
+                state <= 38;
+            end
+
+            38: begin
+                state <= 39;
+            end
+
+            39: begin
+                state <= 40;
+            end
+
+            40: begin
+                state <= 1;
+            end
+
+            41: begin: GO_TO_27
+                state <= 42;
+            end
+
+            42: begin: GO_TO_28
+                state <= 43;
+            end
+
+            43: begin: GO_TO_START_28
                 state <= 1;
             end
 
@@ -268,6 +360,110 @@ module controladora (
                 TwoBytes = 0;
                 PCSrc    = 3;
                 PCWrite  = 1;
+            end
+
+            25: begin: SLL_SRA_SRL_START
+                ShiftQnt    = 2'b01;
+                ShiftReg    = 2'b10;
+                ShiftType   = 3'b001;
+                ALUOutWrite = 0;
+            end
+
+            26: begin:
+                ShiftType = 3'b010;
+            end
+
+            27: begin
+                ShiftType = 3'b100;
+            end
+
+            28: begin
+                ShiftType = 3'b011;
+            end
+
+            29: begin
+                RegDst = 01
+                MemtoReg = 0011
+                RegWrite = 1
+            end
+
+            30: begin: SLLV_SRAV_START
+                ShiftQnt    = 2'b11;
+                ShiftReg    = 2'b01;
+                ShiftType   = 3'b001;
+                ALUOutWrite = 0;
+            end
+
+            31: begin
+                ShiftType = 3'b010;
+            end
+
+            32: begin
+                ShiftType = 3'b011;
+            end
+
+            33: begin: SLL_SRA_SRL_SLLV_SRAV_END
+                RegDst = 2'b01;
+                MemtoReg = 4'b0011;
+                RegWrite = 1;
+            end
+
+            34: begin: SRAM_START
+                RegALoad    = 0;
+                RegBLoad    = 0;
+                ALUSrcA     = 1;
+                ALUSrcB     = 2;
+                ALUOp       = 1;
+                ALUOSrc     = 0;
+                ALUOutWrite = 1;
+            end
+
+            35: begin: SRAM_START
+                ALUOutWrite = 0;
+                IorD        = 1;
+                MemR_W      = 0;
+            end
+
+            36: begin: SRAM_START
+                // wait
+            end
+
+            37: begin: SRAM_START
+                MDRLoad = 1;
+            end
+
+            38: begin: SRAM_START
+                MDRLoad   = 0;
+                ShiftQnt  = 0;
+                ShiftReg  = 2;
+                ShiftType = 1;
+            end
+
+            39: begin: SRAM_START
+                ShiftType = 4;
+            end
+
+            40: begin: SRAM_START
+                RegDst   = 0;
+                MemtoReg = 4'b0011;
+                RegWrite = 1;
+            end
+
+            41: begin: LUI_START
+                ShiftQnt    = 2'b10;
+                ShiftReg    = 2'b00;
+                ShiftType   = 3'b001;
+                ALUOutWrite = 0;
+            end
+
+            42: begin: LUI_SELECTION
+                ShiftType = 010;
+            end
+
+            43: begin: SRAM_LUI_END
+                RegDst = 2'b00;
+                MemtoReg = 4'b0011;
+                RegWrite = 1;
             end
             
         endcase
