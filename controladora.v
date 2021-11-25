bc157c6efcc42b9f9b5df16f1e0cf066a3ab832e
casg <80776185+Caue-ASG@users.noreply.github.com>
2021-11-25 18:08:09 -0300

    só vai

--- controladora.v
+++ controladora.v
@@ -27,7 +27,7 @@ module controladora (
               srl_funct  = 6'h2,
               sram_op    = 6'h1,
               lui_op     = 6'hf;
-    
+
     parameter add_funct = 6'h20,
               and_funct = 6'h24,
               addi_op   = 6'h8,
@@ -45,6 +45,20 @@ module controladora (
               sh_op = 6'h29,
               sw_op = 6'h2b;
 
+    // branchs and jumps opcodes
+    parameter beq_op   = 6'h4,
+              bne_op   = 6'h5,
+              ble_op   = 6'h6,
+              bgt_op   = 6'h7,
+              j_op     = 6'h2,
+              jal_op   = 6'h3;
+
+    // break, rte e jr
+    parameter break_fun = 6'hd,
+              rte_fun   = 6'h13,
+              jr_fun    = 6'h8;
+
+
     reg [7:0] state;
     // if you want for N cycles, set the counter to N-1
     // and go to a state where you change the state if the
@@ -53,7 +67,7 @@ module controladora (
 
     always @(posedge clk) begin: NEXT_STATE_LOGIC
         if(counter > 0)
-            counter <= counter - 1;
+            conter <= counter - 1;
 
         case(state)
             0: begin: GO_TO_1
@@ -85,20 +99,46 @@ module controladora (
                     state <= 15;  // any load or store
 
                 else if((opcode == op0) && ((funct == sll_funct) || (funct == sra_funct) || (funct == srl_funct)))
-                    state = 25;  // sll, sra or srl
+                    state = 20;  // sll, sra or srl
 
                 else if((opcode == op0) && ((funct == sllv_funct) || (funct == srav_funct)))
-                    state = 30;  // sllv, srav
+                    state = 22;  // sllv, srav
 
                 else if(opcode == sram_op)
-                    state = 34;  // sram
+                    state = 25;  // sram
 
                 else if(opcode == lui_op)
-                    state = 41;  // lui
+                    state = 26;  // lui
 
                 else if((opcode == addi_op) || (opcode == addiu_op))
                     state <= 73; // addi or addiu
-                    
+                
+                else if((opcode == beq_op) ||
+                        (opcode == bne_op))
+                    state <= 53; // beq || bne
+
+                else if((opcode == bgt_op) ||
+                        (opcode == ble_op))
+                    state <= 56; // beq || bne
+                
+
+                else if(opcode == j_op)
+                    state <= 60;
+
+                else if(opcode == jal_op)
+                    state <= 59;
+
+                //jr
+                else if((opcode == op0) && (funct == jr_fun))
+                    state <= 61;
+
+                //break       
+                else if((opcode == op0) && (funct == break_fun))
+                    state <= 51;
+                //rte
+                else if((opcode == op0) && (funct == rte_fun))
+                    state <= 52;
+
                 else // opcode inexistente
                     state <= 11;
             end
@@ -157,7 +197,7 @@ module controladora (
                 if(opcode == lw_op)
                     state <= 20;
                 else if(opcode == lh_op)
-                    state <= 21;
+                    sate <= 21;
                 else if(opcode == lb_op)
                     state <= 22;
                 else if(opcode == sh_op)
@@ -276,6 +316,55 @@ module controladora (
                 else
                     state <= 9;  // addiu
             end
+
+            53: begin
+                if(opcode == beq_op)
+                    state <= 54; // beq
+                else
+                    state <= 55; // bne
+            end 
+
+            54: begin
+                state <= 1;
+            end
+
+            55: begin
+                state <= 1;
+            end
+
+            56: begin
+                if(opcode == bgt_op)
+                    state <= 57; // bgt
+                else
+                    state <= 58; // ble
+            end 
+
+            57: begin
+                state <= 1;
+            end
+            
+            58: begin
+                state <= 1;
+            end
+
+            59: begin //jal_beginning
+                state <= 60; 
+            end
+            60: begin //jump or jal_end
+                state <= 1; 
+            end
+
+            61: begin 
+                state <= 1;
+            end
+
+            51: begin
+                state <= 1;
+            end
+
+            52: begin 
+                state <= 1
+            end 
 
             default: state <= 0;
         endcase
@@ -453,16 +542,16 @@ module controladora (
             end
 
             16: begin
-                IorD          = 1;
-                MemRead_Write = 0;
-                ALUOutWrite   = 0;
+                IorD         = 1;
+                MemReadWrite = 0;
+                ALUOutWrite  = 0;
             end
 
             17: begin
-                WDSrc         = 0;
-                IorD          = 1;
-                MemRead_Write = 1;
-                ALUOutWrite   = 1;
+                WDSrc        = 0;
+                IorD         = 1;
+                MemReadWrite = 1;
+                ALUOutWrite  = 1;
             end
 
             18: begin
@@ -499,21 +588,21 @@ module controladora (
             end
 
             23: begin
-                Store         = 1;
-                TwoBytes      = 1;
-                WDSrc         = 1;
-                IorD          = 1;
-                MemRead_Write = 1;
-                MDRLoad       = 0;
+                Store        = 1;
+                TwoBytes     = 1;
+                WDSrc        = 1;
+                IorD         = 1;
+                MemReadWrite = 1;
+                MDRLoad      = 0;
             end
 
             24: begin
-                Store         = 1;
-                TwoBytes      = 0;
-                WDSrc         = 1;
-                IorD          = 1;
-                MemRead_Write = 1;
-                MDRLoad       = 0;
+                Store        = 1;
+                TwoBytes     = 0;
+                WDSrc        = 1;
+                IorD         = 1;
+                MemReadWrite = 1;
+                MDRLoad      = 0;
             end
 
             25: begin: SLL_SRA_SRL_START
@@ -523,7 +612,7 @@ module controladora (
                 ALUOutWrite = 0;
             end
 
-            26: begin
+            26: begin:
                 ShiftType = 3'b010;
             end
 
@@ -536,9 +625,9 @@ module controladora (
             end
 
             29: begin
-                RegDst = 2'b01;
-                MemtoReg = 4'b0011;
-                RegWrite = 1;
+                RegDst = 01
+                MemtoReg = 0011
+                RegWrite = 1
             end
 
             30: begin: SLLV_SRAV_START
@@ -553,7 +642,7 @@ module controladora (
             end
 
             32: begin
-                ShiftType = 3'b100;
+                ShiftType = 3'b011;
             end
 
             33: begin: SLL_SRA_SRL_SLLV_SRAV_END
@@ -562,7 +651,7 @@ module controladora (
                 RegWrite = 1;
             end
 
-            34: begin: SRAM_START_34
+            34: begin: SRAM_START
                 RegALoad    = 0;
                 RegBLoad    = 0;
                 ALUSrcA     = 1;
@@ -572,32 +661,32 @@ module controladora (
                 ALUOutWrite = 1;
             end
 
-            35: begin: SRAM_START_35
-                ALUOutWrite   = 0;
-                IorD          = 1;
-                MemRead_Write = 0;
+            35: begin: SRAM_START
+                ALUOutWrite = 0;
+                IorD        = 1;
+                MemReadWrite      = 0;
             end
 
-            36: begin: SRAM_START_36
+            36: begin: SRAM_START
                 // wait
             end
 
-            37: begin: SRAM_START_37
+            37: begin: SRAM_START
                 MDRLoad = 1;
             end
 
-            38: begin: SRAM_START_38
+            38: begin: SRAM_START
                 MDRLoad   = 0;
                 ShiftQnt  = 0;
                 ShiftReg  = 2;
                 ShiftType = 1;
             end
 
-            39: begin: SRAM_START_39
+            39: begin: SRAM_START
                 ShiftType = 4;
             end
 
-            40: begin: SRAM_START_40
+            40: begin: SRAM_START
                 RegDst   = 0;
                 MemtoReg = 4'b0011;
                 RegWrite = 1;
@@ -611,7 +700,7 @@ module controladora (
             end
 
             42: begin: LUI_SELECTION
-                ShiftType = 3'b010;
+                ShiftType = 010;
             end
 
             43: begin: SRAM_LUI_END
@@ -628,6 +717,79 @@ module controladora (
                 ALUOutWrite = 1;
             end
             
+            // caue:
+            53: begin: BEQ_OR_BNE
+                ALUSrcA     = 1;
+                ALUSrcB     = 0;
+                ALUOp       = 3'b010;
+                PCSrc       = 1;
+                ALUOutWrite = 0;
+            end
+
+            54: begin: BEQ
+                EQorNE      = 1;
+                PCWriteCond = 1;
+            end
+
+            55: begin: BNE
+                EQorNE      = 0;
+                PCWriteCond = 1;
+            end
+
+
+            56: begin: BGT_OR_BLE
+                ALUSrcA     = 1;
+                ALUSrcB     = 0;
+                ALUOp       = 3'b111;
+                PCSrc       = 1;
+                ALUOutWrite = 0;
+            end
+
+            57: begin: BGT
+                GTorLT      = 1;
+                PCWriteCond = 1;
+            end
+
+            58: begin: BLE
+                GTorLT      = 0;
+                PCWriteCond = 1;
+            end
+
+            59: begin: JAL
+                MemtoReg = 6;
+                RegWrite = 1;
+                ALUOutWrite = 0;
+            end
+
+            60: begin: Jump_or_Jal
+                PCSrc = 2;
+                PCWrite = 1;
+                RegWrite = 0;
+                ALUOutWrite = 0;
+            end
+
+            61: begin: JR
+                ALUSrcA = 1;
+                ALUOp = 3'b000;
+                PCSrc = 0;
+                PCWrite = 1;
+                ALUOutWrite = 0;
+            end
+
+            51: begin: Break
+                ALUSrcA = 0;
+                ALUSrcB = 1;
+                ALUOp = 3'b010;
+                PCSrc = 0;
+                PCWrite = 1;
+                ALUOutWrite = 0;                
+            end
+
+            52: begin: rte
+                PCSrc = 4;
+                PCWrite = 1;
+                ALUOutWrite = 0;
+            end
         endcase
     end
 
