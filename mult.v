d7a42d623b821547a16238d37ab4a68fd66bac22
EdGeber <egma@cin.ufpe.br>
2021-11-26 21:35:14 -0300

    Bug fixes

--- mult.v
+++ mult.v
@@ -8,15 +8,16 @@ module mult (
 parameter nbits = 32;
 
 reg [6:0] counter;
-reg signed [31:0] mA;
+reg signed [31:0] signed_a;
 reg signed [64:0] A,S,P;
+reg [63:0] result;
 reg rodando;
 
 always @(posedge clk or posedge rst or posedge init or posedge stop) begin
     if(init) begin
         rodando <= 1;
         counter <= 33;
-        mA <= a;
+        signed_a <= a;
     end
     if(stop || rst) begin
         rodando <= 1'b0;
@@ -30,7 +31,7 @@ always @(posedge clk or posedge rst or posedge init or posedge stop) begin
     if(rodando) begin
         if(counter == 33) begin
             A <= {a,{33{1'b0}}};
-            S <= {~(-mA)+1, {33{1'b0}}};
+            S <= {-signed_a, {33{1'b0}}};
             P <= {{{32{1'b0}},b},1'b0};
             counter <= counter - 1;
         end
@@ -39,7 +40,9 @@ always @(posedge clk or posedge rst or posedge init or posedge stop) begin
             if(P[1:0] == 2'b01) 
                 P <= (P + A) >>> 1;
             else if(P[1:0] == 2'b10)
-                P <= (P + S) >>> 1;     
+                P <= (P + S) >>> 1;
+            else
+                P <= P >>> 1;
             counter <= counter - 1;
         end
         
@@ -47,6 +50,7 @@ always @(posedge clk or posedge rst or posedge init or posedge stop) begin
             hi <= P[64:33];
             lo <= P[32:1];
             rodando <= 1'b0;
+            result <= P[64:1];
         end
     end
 end
